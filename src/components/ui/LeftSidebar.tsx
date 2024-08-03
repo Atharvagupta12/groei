8dc978e44765a023d022ea7814160a66d341bad6
Atharvagupta12 <atharvguptaofficial@gmail.com>
2024-08-03 23:51:48 +0530

    Left sidebar done

--- src/components/ui/LeftSidebar.tsx
+++ src/components/ui/LeftSidebar.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { sidebarLinks } from "../../../constants";
+import { cn } from "@/lib/utils";
+import { usePathname, useRouter } from "next/navigation";
+
 
 const LeftSidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6">
@@ -13,16 +21,27 @@ const LeftSidebar = () => {
         >
           <Image src="/icons/logo.svg" alt="logo" width={23} height={23} />
           <h1 className="text-24 font-extrabold text-white max:lg:hidden">
-            Gorei
+            Groei
           </h1>
         </Link>
 
-        {sidebarLinks.map(({route, label}) => {
-            return <Link href={route}>
-            {label}
+        {sidebarLinks.map(({ route, label, imgURL }) => {
+          const isActive =
+            pathname === route || pathname.startsWith(`${route}/`);
+
+          return (
+            <Link
+              href={route}
+              key={label}
+              className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start ",{
+                'bg-nav-focus border-r-4 border-orange-1': isActive
+              })}
+            >
+              <Image src={imgURL} alt={label} width={24} height={24} />
+              <p>{label}</p>
             </Link>
-        })
-        }
+          );
+        })}
       </nav>
     </section>
   );
