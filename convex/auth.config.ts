8e9d714e2be293c33db013351abccf5a12adf691
Atharvagupta12 <atharvguptaofficial@gmail.com>
2024-08-07 23:47:38 +0530

    auth and tasks done

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://renewed-coyote-90.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
--- convex/schema.ts
+++ convex/schema.ts
@@ -22,7 +22,7 @@ export default defineSchema({
   })
   .searchIndex('search_author', {searchField: 'author' })
   .searchIndex('search_title', {searchField: 'podcastTitle' })
-  .searchIndex('search_body', {searchField: 'podcastDescription' })
+  .searchIndex('search_body', {searchField: 'podcastDescription' }),
   users: defineTable({
     email: v.string(),
     imageUrl: v.string(),
