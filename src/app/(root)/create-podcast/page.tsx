2cc4531c8df29eb2e0be41fba25294be4efdafd8
Atharvagupta12 <atharvguptaofficial@gmail.com>
2024-08-09 22:59:53 +0530

    updated but more to go

--- src/app/(root)/create-podcast/page.tsx
+++ src/app/(root)/create-podcast/page.tsx
@@ -2,13 +2,9 @@ import React from "react";
 
 const CreatePodcast = () => {
   return (
-<<<<<<< HEAD
     <div>
       <h1 className="text-20 font-bold text-white-1">Trending Podcast</h1>
-=======
-    <div className='text-white-1'>
-      Create Podcast
->>>>>>> 29f0869985219850409294b7b3da93c6c2b9e1b1
+      <div className="text-white-1">Create Podcast</div>
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ConvexClientProvider } from "./providers/convexProvider";
+import ConvexClerkProvider from "./providers/ConvexClerkProvider";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        <ConvexClerkProvider>{children}</ConvexClerkProvider> 
       </body>
     </html>
   );
