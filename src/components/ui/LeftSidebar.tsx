73650fdd7d490a8aaadf47b889b36c688edcb335
Atharvagupta12 <atharvguptaofficial@gmail.com>
2024-08-14 23:59:40 +0530

    left-sidebar color changed

--- src/components/ui/LeftSidebar.tsx
+++ src/components/ui/LeftSidebar.tsx
@@ -34,7 +34,7 @@ const LeftSidebar = () => {
               href={route}
               key={label}
               className={cn("flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start ",{
-                'bg-nav-focus border-r-4 border-orange-1': isActive
+                'bg-nav-focus border-r-4 border-violet-600': isActive
               })}
             >
               <Image src={imgURL} alt={label} width={24} height={24} />
