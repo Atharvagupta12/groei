a88f1b33602ad34e492686dd2b0528fb186ef448
Atharvagupta12 <atharvguptaofficial@gmail.com>
2024-09-10 17:17:24 +0530

    profile page done

--- src/app/(root)/profile/[profileID]/page.tsx
+++ src/app/(root)/profile/[profileID]/page.tsx
@@ -8,60 +8,58 @@ import ProfileCard from "@/components/ui/ProfileCard";
 import { api } from "../../../../../convex/_generated/api";
 
 const ProfilePage = ({
-  params: {profileId} }: {
+  params }: {
   params: {
     profileId: string;
   }
 }) => {
-  // const user = useQuery(api.users.getUserById, {
-  //   clerkId: profileId,
-  // });
-  // const podcastsData = useQuery(api.podcasts.getPodcastByAuthorId, {
-  //   authorId:profileId,
-  // });
+  const user = useQuery(api.users.getUserById, {
+    clerkId: params.profileId,
+  });
+  const podcastsData = useQuery(api.podcasts.getPodcastByAuthorId, {
+    authorId: params.profileId,
+  });
 
-  // if (!user || !podcastsData) return <LoaderSpinner />;
+  if (!user || !podcastsData) return <LoaderSpinner />;
 
   return (
-<h1 className="flex justify-center text-white-1 mt-10" >
-  User is not available now ,will fix it soon</h1>
 
-    // <section className="mt-9 flex flex-col">
-    //   <h1 className="text-20 font-bold text-white-1 max-md:text-center">
-    //     Groei Profile
-    //   </h1>
-    //   <div className="mt-6 flex flex-col gap-6 max-md:items-center md:flex-row">
-    //     <ProfileCard
-    //       podcastData={podcastsData!}
-    //       imageUrl={user?.imageUrl!}
-    //       userFirstName={user?.name!}
-    //     />
-    //   </div>
-    //   <section className="mt-9 flex flex-col gap-5">
-    //     <h1 className="text-20 font-bold text-white-1">All Podcasts</h1>
-    //     {podcastsData && podcastsData.podcasts.length > 0 ? (
-    //       <div className="podcast_grid">
-    //         {podcastsData?.podcasts
-    //           ?.slice(0, 4)
-    //           .map((podcast) => (
-    //             <PodcastCard
-    //               key={podcast._id}
-    //               imgUrl={podcast.imageUrl!}
-    //               title={podcast.podcastTitle!}
-    //               description={podcast.podcastDescription}
-    //               podcastId={podcast._id}
-    //             />
-    //           ))}
-    //       </div>
-    //     ) : (
-    //       <EmptyState
-    //         title="You have not created any podcasts yet"
-    //         buttonLink="/create-podcast"
-    //         buttonText="Create Podcast"
-    //       />
-    //     )}
-    //   </section>
-    // </section>
+    <section className="mt-9 flex flex-col">
+      <h1 className="text-20 font-bold text-white-1 max-md:text-center">
+        Groei Profile
+      </h1>
+      <div className="mt-6 flex flex-col gap-6 max-md:items-center md:flex-row">
+        <ProfileCard
+          podcastData={podcastsData!}
+          imageUrl={user?.imageUrl!}
+          userFirstName={user?.name!}
+        />
+      </div>
+      <section className="mt-9 flex flex-col gap-5">
+        <h1 className="text-20 font-bold text-white-1">All Podcasts</h1>
+        {podcastsData && podcastsData.podcasts.length > 0 ? (
+          <div className="podcast_grid">
+            {podcastsData?.podcasts
+              ?.slice(0, 4)
+              .map((podcast) => (
+                <PodcastCard
+                  key={podcast._id}
+                  imgUrl={podcast.imageUrl!}
+                  title={podcast.podcastTitle!}
+                  description={podcast.podcastDescription}
+                  podcastId={podcast._id}
+                />
+              ))}
+          </div>
+        ) : (
+          <EmptyState
+            title="You have not created any podcasts yet"
+            buttonLink="/create-podcast"
+            buttonText="Create Podcast"
+          />
+        )}
+      </section>
+    </section>
   );
 };
 
